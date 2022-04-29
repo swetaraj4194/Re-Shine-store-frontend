4d840487794158209b98824bae71ca2e37ffa163
swetaraj4194 <swetaraj4194@gmail.com>
2022-04-29 14:16:52 +0200

    added heroku url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4002";
+export const apiUrl =
+  process.env.API_URL || "https://re-shine-store-server.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
